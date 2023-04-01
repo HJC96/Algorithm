b71a1f2fc3076621ddcc8aa9643178d5ffe78888
Han Ji Chan <gkswlcjs2@naver.com>
2023-04-01 18:51:52 +0900

    Update 연속된 수의 합.cpp

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.cpp
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.cpp
@@ -3,32 +3,31 @@
 
 using namespace std;
 
-int sum_(vector<int> num)
-{
+int sum_(const vector<int>& num) {
     int ans = 0;
-    for(int i=0;i<num.size();i++)
+    for (int i = 0; i < num.size(); i++)
         ans += num[i];
     return ans;
 }
 
 vector<int> solution(int num, int total) {
     vector<int> answer;
-    for(int i=1;i<=num;i++)
+
+    // 초기 범위 설정
+    for (int i = 1; i <= num; i++)
         answer.push_back(i);
-    while(sum_(answer) < total)
-    {
+
+    // 총합이 total보다 작은 경우 범위를 오른쪽으로 이동
+    while (sum_(answer) < total) {
         answer.erase(answer.begin());
         answer.push_back(++num);
     }
-    if(sum_(answer) == total)
-        return answer;
-    else
-    {
-        while(sum_(answer) != total)
-        {
-            answer.pop_back();
-            answer.insert(answer.begin(),answer[0]-1);
-        }
-        return answer;
+
+    // 총합이 total이 아닌 경우 범위를 왼쪽으로 이동
+    while (sum_(answer) != total) {
+        answer.pop_back();
+        answer.insert(answer.begin(), answer[0] - 1);
     }
+
+    return answer;
 }
