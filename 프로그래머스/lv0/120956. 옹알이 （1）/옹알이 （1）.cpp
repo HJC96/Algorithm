8f7d7e6def29d251dec780734ea73b6c1b7705c8
Han Ji Chan <gkswlcjs2@naver.com>
2023-04-02 20:55:49 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.cpp
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.cpp
@@ -1,39 +1,10 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-#include <set>
+
+#define abs(a) ((a)>(0)?(a):(-a))
 using namespace std;
-int result = 0;
-int visited[4] = {0,};
-vector<string> baby = {"aya", "ye", "woo", "ma"};
-void solve(string st, vector<string> bab)
-{
-    while(find(bab.begin(),bab.end(),st) != bab.end())
-    {
-        result++;
-        bab.erase(find(bab.begin(),bab.end(),st));
-    }
-    for(int i=0;i<baby.size();i++)
-    {
-        if(visited[i] == 1)
-            continue;
-        visited[i] = 1;
-        solve(st+baby[i],bab);
-        visited[i] = 0;
-    }
-    return ;
-}
+
 int solution(vector<string> babbling) {
-    set<string> s;
-    for(auto elem:babbling)
-        s.insert(elem);
-    for(int i=0;i<baby.size();i++)
-    {
-        visited[i] = 1;
-        solve(baby[i], babbling);
-        visited[i] = 0;
-    }
-    
-    return result;
+    int answer = -1*(-1)/(abs(-1));
+    return answer;
 }
