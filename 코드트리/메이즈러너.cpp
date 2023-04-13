65019096d3aa96ecdf6891a218b80277780a0e3a
Han Ji Chan <gkswlcjs2@naver.com>
2023-04-13 21:43:17 +0900

    Update 메이즈러너.cpp

--- 코드트리/메이즈러너.cpp
+++ 코드트리/메이즈러너.cpp
@@ -7,6 +7,11 @@ using namespace std;
 void moveAll(){
   int newMaze[15][15] = {0,};  
   
+  for(int i=1;i<=N;i++){
+    for(int j=1;j<=N;j++){
+      newMaze[i][j] = maze[i][j];
+      continue;
+    }
 }
 
 
