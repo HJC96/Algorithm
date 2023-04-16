ac9ad40cd45d90202410919b4742364d1ae16726
Han Ji Chan <gkswlcjs2@naver.com>
2023-04-16 19:20:12 +0900

    [level 2] Title: 타겟 넘버, Time: 0.37 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,18 @@
+class Solution {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+        DFS(0,target,numbers, 0);
+        return answer;
+    }
+    public void DFS(int cur_int,int target,int[] n, int idx){
+        if(idx == n.length){
+            if(cur_int == target){ 
+                answer++;
+            }
+            return;
+        }
+
+        DFS(cur_int + n[idx],target,n,idx+1);
+        DFS(cur_int - n[idx],target,n,idx+1);
+    }
+}
