5da3384f0a1b6013e115b5f3ee5afaa30863d3a5
Han Ji Chan <gkswlcjs2@naver.com>
2023-04-16 16:59:30 +0900

    [Silver II] Title: 최소 힙, Time: 356 ms, Memory: 27632 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0;i<T;i++){
+            long num = Long.parseLong((br.readLine()));
+            if(num == 0){
+                if(pq.isEmpty()){
+                    sb.append("0\n");
+                    continue;
+                }
+                sb.append(pq.poll() + "\n");
+            }
+            else{
+                pq.add(num);
+            }
+        }
+        System.out.print(sb);
+    }
+}
