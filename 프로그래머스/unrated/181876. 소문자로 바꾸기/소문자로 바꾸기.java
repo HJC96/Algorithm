b9dfd39262547f775aa690b15d860e37990cff35
Han Ji Chan <gkswlcjs2@naver.com>
2023-04-20 23:26:18 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        answer = myString.toLowerCase();
+        return answer;
+    }
+}
