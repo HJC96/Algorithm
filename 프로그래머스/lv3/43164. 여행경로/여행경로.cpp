3f7e128608b115f2e79d73ba89790260bf8d85e4
Han Ji Chan <gkswlcjs2@naver.com>
2023-05-01 16:21:56 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.cpp
@@ -21,11 +21,8 @@ void dfs(string cur_pos, vector<string> ans) {
 
         for (int idx = 0; idx < h[cur_pos].size(); idx++) {
             string next_pos = h[cur_pos][idx].first;
-            // for (int i = 0; i < h[next_pos].size(); i++) {
             int visited = h[cur_pos][idx].second;
                 if (!visited) {
-                    //cout << idx << cur_pos << next_pos << h[next_pos].second << endl;
-
                     h[cur_pos][idx].second = 1;
                     dfs(next_pos, ans);
                     h[cur_pos][idx].second = 0;
@@ -57,55 +54,3 @@ vector<string> solution(vector<vector<string>> tickets) {
     return answer;
 }
 
-
-/*
-#include <string>
-#include <vector>
-#include <map>
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-map<string,vector<string>> h;
-int ticket_use = 0;
-vector<string> dfs(string cur_pos, vector<string> ans){
-    cout << cur_pos << endl;
-    ans.push_back(cur_pos);
-    if(ans.size() == ticket_use+1)
-        return ans;
-    if(h[cur_pos].size()<=0) {ans.clear(); return ans;}
-    int idx=0;
-    while(h[cur_pos].size()>0){
-        string next_pos = h[cur_pos][idx];
-        //ans.push_back(next_pos);
-        h[cur_pos].erase(h[cur_pos].begin());
-        dfs(next_pos, ans);
-    }
-    return ans;
-}
-
-vector<string> solution(vector<vector<string>> tickets) {
-
-    for(auto elem:tickets){
-        if(h.count(elem[0])>0){ // if(h[elem[0]].size() > 0)를 사용하면 안됨. 키 'elem[0]'가 없는 경우 예외 발생
-            h[elem[0]].push_back(elem[1]);
-        }
-        else{
-            vector<string> tmp;
-            tmp.push_back(elem[1]);
-            h.insert({elem[0],tmp});
-        }
-    }
-
-    for(auto &e:h){
-         sort((e.second).begin(),(e.second).end());
-         ticket_use += e.second.size();
-    }
-    vector<string> tmp;
-    //answer.push_back("ICN");
-
-    return dfs("ICN", tmp);
-}
-
-*/
