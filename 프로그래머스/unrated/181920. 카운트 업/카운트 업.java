8907d64145527dd413219882d50443db62d4f287
Han Ji Chan <gkswlcjs2@naver.com>
2023-05-14 13:28:15 +0900

    [unrated] Title: 카운트 업, Time: 0.01 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[end-start+1];
+        for(int i=start;i<=end;i++)
+            answer[i-start] = i;
+        return answer;
+    }
+}
