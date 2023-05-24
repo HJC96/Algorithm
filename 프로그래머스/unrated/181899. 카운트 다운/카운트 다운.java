680b60c5efb13aef6fa25a39457baa38552e1790
Han Ji Chan <gkswlcjs2@naver.com>
2023-05-24 19:10:00 +0900

    [unrated] Title: 카운트 다운, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[start-end+1];
+        for(int i=start;i>=end;i--)
+            answer[start-i] = i;
+        return answer;
+    }
+}
