eae66ebcbb1c7c734eaa2ebc8d4efa6e41895bac
Han Ji Chan <gkswlcjs2@naver.com>
2023-05-29 09:10:59 +0900

    [unrated] Title: rny_string, Time: 9.30 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        for(int i=0;i<rny_string.length();i++){            
+            if(rny_string.charAt(i) == 'm'){
+                answer += "rn";
+                continue;
+            }
+            answer += rny_string.charAt(i);
+                
+        }
+        return answer;
+    }
+}
