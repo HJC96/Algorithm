7cc2fc89cfd337cd0381a59ff6009d127565997c
Han Ji Chan <gkswlcjs2@naver.com>
2023-06-06 11:22:48 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.04 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, String control) {
+        for(char c : control.toCharArray()) {
+            switch(c){
+                case 'w':
+                    n += 1;
+                    break;
+                case 's':
+                    n -= 1;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return n;
+    }
+}
