0594fd2d0bcce0a4eb618c370be38a6f074e3fc2
Han Ji Chan <gkswlcjs2@naver.com>
2023-07-30 11:28:12 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.18 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        Stack<Character> stack = new Stack<>();
+        char[] result = new char[number.length() - k];
+        
+        for(int i = 0; i < number.length(); i++){
+            while(!stack.isEmpty() && stack.peek() < number.charAt(i) && k > 0){
+                stack.pop();
+                k--;
+            }
+            stack.push(number.charAt(i));
+        }
+        
+        for(int i = 0; i < result.length; i++){
+            result[i] = stack.get(i);
+        }
+        
+        return new String(result);
+    }
+}
