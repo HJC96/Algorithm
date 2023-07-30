64d95d7a6880cc30cdd888fac72ae77831f27ae3
Han Ji Chan <gkswlcjs2@naver.com>
2023-07-30 11:41:04 +0900

    [level 2] Title: 구명보트, Time: 9.65 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/README.md
+++ 프로그래머스/lv2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=cpp#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
 
 ### 성능 요약
 
-메모리: 4.62 MB, 시간: 1.95 ms
+메모리: 53.6 MB, 시간: 9.65 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int front=0; int end=people.length-1;
+        while(front<=end){ // 추후 등호 고려
+            if(people[front]+people[end] > limit){
+                answer++;
+                end-=1;
+            }else{
+                front++;
+                end-=1;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
