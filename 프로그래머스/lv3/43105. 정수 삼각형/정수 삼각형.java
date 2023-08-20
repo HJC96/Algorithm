a88038a32d092505db6802fb12efad541b9ca8a6
Han Ji Chan <gkswlcjs2@naver.com>
2023-08-21 05:21:11 +0900

    [level 3] Title: 정수 삼각형, Time: 10.90 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int length = triangle.length;
+        int[][] map = new int[length][length];
+        map[0][0] = triangle[0][0];
+        for(int i=1;i<length;i++){
+            for(int j=0;j<triangle[i].length;j++){
+                if(1<=j && j<triangle[i].length-1 ){
+                   map[i][j] = Math.max(map[i-1][j-1],map[i-1][j]) + triangle[i][j];
+                    if(map[i][j] >= answer) answer = Math.max(answer, map[i][j]);
+                }
+                else{
+                    if(j==0)
+                        map[i][j] = map[i-1][j] + triangle[i][j];
+                    else 
+                        map[i][j] = map[i-1][j-1] + triangle[i][j];
+                    if(map[i][j] >= answer) answer = Math.max(answer, map[i][j]);
+                }
+            }
+        }
+        return answer;
+    }
+}
