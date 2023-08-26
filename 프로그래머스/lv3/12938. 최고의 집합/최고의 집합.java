1799b02dbcc40acabd063304894ee9b2beefceae
Han Ji Chan <gkswlcjs2@naver.com>
2023-08-26 12:03:03 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -5,18 +5,23 @@ class Solution {
     public int[] solution(int n, int s) {
         if (s < n) return new int[]{-1}; // 불가능한 경우
         
-        int baseValue = s / n;
+        int Quotient = s / n;
         int remainder = s % n;
 
         int[] answer = new int[n];
         for (int i = 0; i < n; i++) {
-            answer[i] = baseValue;
+            answer[i] = Quotient;
         }
-
-        // 나머지만큼 1씩 더해 분포시키기
-        for (int i = 0; i < remainder; i++) {
-            answer[n - 1 - i] += 1; // 뒤에서부터 1을 더해 큰 값을 만든다
+        
+        int idx=0;
+        while(remainder > 0){
+            if(idx >= n) idx=0;
+            answer[idx++]++;
+            remainder--;
         }
+        
+        Arrays.sort(answer);
+        
         
         return answer;
     }
