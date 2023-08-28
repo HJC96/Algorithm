0290cd3678023e1bc149ff94315f9adb27eb99da
Han Ji Chan <gkswlcjs2@naver.com>
2023-08-29 07:48:49 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 0.41 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/lv3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -3,67 +3,54 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] lines) {
-        int []time = new int[236000000];
         int answer=0;
-        int tmp_answer = 0;
-        List<Integer> start = new ArrayList<>();
-        List<Integer> end = new ArrayList<>();
+        List<Integer> startTimes = new ArrayList<>();
+        List<Integer> endTimes = new ArrayList<>();
+        
         for(String line:lines){
-            String tmp_1[] = line.split(" ");
-            String tmp_2[] = tmp_1[1].split(":");
-            String tmp_3[] = tmp_2[2].split("\\.");
+            String[] spaceSplit = line.split(" ");
+            String[] colonSplit = spaceSplit[1].split(":");
+            String[] dotSplit = colonSplit[2].split("\\.");
             
-            int hour = Integer.parseInt(tmp_2[0]);
-            int minute = Integer.parseInt(tmp_2[1]);
-            int second = Integer.parseInt(tmp_3[0]);
-            int millisecond = Integer.parseInt(tmp_3[1]);
+            int hour = Integer.parseInt(colonSplit[0]);
+            int minute = Integer.parseInt(colonSplit[1]);
+            int second = Integer.parseInt(dotSplit[0]);
+            int millisecond = Integer.parseInt(dotSplit[1]);
             
             int endTime = ((hour * 60 + minute) * 60 + second) * 1000 + millisecond;
+            int processDuration = (int) (Float.parseFloat(spaceSplit[2].substring(0, spaceSplit[2].length() - 1)) * 1000);
+            int startTime = endTime - processDuration + 1;
             
-            int processTime = (int)(Float.parseFloat(tmp_1[2].substring(0,tmp_1[2].length()-1))*1000) ;
-            int startTime = endTime - processTime + 1;
-            
-            start.add(startTime);
-            end.add(endTime);
+            startTimes.add(startTime);
+            endTimes.add(endTime);
         }
         
-        for(int i=0;i<lines.length;i++){
-            int startRange = end.get(i);
-            int endRange = startRange + 1000;
+        for (int i = 0; i < lines.length; i++) {
+            int startRange = endTimes.get(i);
+            int endRange = startRange + 1000; // 1000ms
 
             int count = 0;
-            for(int j=0;j<lines.length;j++){
-                if(start.get(j) < endRange && end.get(j) >= startRange)
-                {
+            for (int j = 0; j < lines.length; j++) {
+                if (startTimes.get(j) < endRange && endTimes.get(j) >= startRange) { // 1001ms 검사하는 것이 아닌, 1000ms 검사이므로 부등호에 유의
                     count++;
                 }
             }
-            answer = Math.max(answer, count); 
-
+            answer = Math.max(answer, count);
         }
-        
-        for(int i=0;i<lines.length;i++){
-            int startRange = start.get(i);
+
+        for (int i = 0; i < lines.length; i++) {
+            int startRange = startTimes.get(i);
             int endRange = startRange + 1000;
+
             int count = 0;
-            for(int j=0;j<lines.length;j++){
-                if(start.get(j) < endRange && end.get(j) >= startRange)
-                {
+            for (int j = 0; j < lines.length; j++) {
+                if (startTimes.get(j) < endRange && endTimes.get(j) >= startRange) {
                     count++;
                 }
             }
-            answer = Math.max(answer, count); 
+            answer = Math.max(answer, count);
         }
-        
 
         return answer;
     }
 }
-
-/*
-좌표로 그린다.
-좌표로 그려서 겹치는 부분을 카운트하면 될것 같은데...
-
-Max T가 3...
-
-*/
