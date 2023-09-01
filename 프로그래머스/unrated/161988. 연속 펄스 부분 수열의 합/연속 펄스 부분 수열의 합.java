962357324dbc5eca37e148260c624fdec2f7f668
Han Ji Chan <gkswlcjs2@naver.com>
2023-09-02 08:47:27 +0900

    [unrated] Title: 연속 펄스 부분 수열의 합, Time: 20.72 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -7,23 +7,32 @@ public class Solution {
         int length = sequence.length;
         long[] dpPos = new long[length];
         long[] dpNeg = new long[length];
+
         
-        dpPos[0] = sequence[0];
-        dpNeg[0] = -sequence[0];
-        
-        answer = Math.max(dpPos[0], dpNeg[0]);
-        for (int i = 1; i < length; i++) {
-            if (i % 2 == 0) {
-                dpPos[i] = Math.max(dpPos[i - 1] + sequence[i], sequence[i]);
-                dpNeg[i] = Math.max(dpNeg[i - 1] - sequence[i], -sequence[i]);
-            } else {
-                dpPos[i] = Math.max(dpPos[i - 1] - sequence[i], -sequence[i]);
-                dpNeg[i] = Math.max(dpNeg[i - 1] + sequence[i], sequence[i]);
-            }
-            
-            answer = Math.max(answer, Math.max(dpPos[i], dpNeg[i]));
+
+
+        int[] purse = new int[length];
+        purse[0] = 1;
+        for(int i=1;i<length;i++){
+            purse[i] = -1*purse[i-1];
         }
-        
+        for(int i=0;i<length;i++) sequence[i] = sequence[i]*purse[i];
+
+        dpPos[0] = sequence[0];
+        dpNeg[0] = sequence[0];
+        answer = Math.max(Math.abs(dpPos[0]), dpNeg[0]);
+        for (int i = 1; i < length; i++) {
+//            if (i % 2 == 0) {
+                dpPos[i] = Math.max(dpPos[i - 1] + sequence[i], sequence[i]);
+                dpNeg[i] = Math.min(dpNeg[i - 1] + sequence[i], sequence[i]);
+//            } else {
+//                dpPos[i] = Math.max(dpPos[i - 1] - sequence[i], -sequence[i]);
+//                dpNeg[i] = Math.max(dpNeg[i - 1] + sequence[i], sequence[i]);
+//            }
+
+            answer = Math.max(answer, Math.max(Math.abs(dpPos[i]), Math.abs(dpNeg[i])));
+        }
+
         return answer;
     }
 }
