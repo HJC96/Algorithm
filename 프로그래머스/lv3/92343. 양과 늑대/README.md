5a788834d2917361b4c41704864a4c150fa7629d
Han Ji Chan <gkswlcjs2@naver.com>
2023-09-06 09:16:53 +0900

    [level 3] Title: 양과 늑대, Time: 1.37 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/README.md
+++ 프로그래머스/lv3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=java#) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 1.37 ms
+메모리: 75.2 MB, 시간: 1.37 ms
 
 ### 구분
 
--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -2,46 +2,47 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public static int dfs(int currNode, int w, int s, List<Integer> nextNodes, int[] info) {
-        int animal = info[currNode];
-        if (animal == 0) {
-            s++;
-        } else {
-            w++;
-        }
-
-        int ans = s;
-
-        if (w >= s) {
-            return ans;
-        }
-
-        for (int i = 0; i < nextNodes.size(); i++) {
-            List<Integer> next = new ArrayList<>(nextNodes);
-            int nextNode = nextNodes.get(i);
-            next.remove((Integer)nextNode);
-            next.addAll(graph.get(nextNode));
-            ans = Math.max(ans, dfs(nextNode, w, s, next, info));
-        }
-
-        return ans;
-    }
 
     static List<List<Integer>> graph = new ArrayList<>();
 
-    public static int solution(int[] info, int[][] edges) {
-        graph.clear();
-        for (int i = 0; i < info.length; i++) {
+    public int dfs(int wolf, int sheep, int CurrNode, List<Integer> nextNodes, int[] info){
+        if(info[CurrNode] == 0) sheep++;
+        else wolf++;
+        
+        int ans = sheep;
+        if(sheep <= wolf)
+            return ans;
+        
+        for(int i=0;i<nextNodes.size();i++){
+            List<Integer> stackNode = new ArrayList<>(nextNodes);
+            int nextNode = nextNodes.get(i);
+            stackNode.remove((Integer)nextNode);
+            stackNode.addAll(graph.get(nextNode));
+            
+            
+            ans = Math.max(ans, dfs(wolf, sheep, nextNode, stackNode, info));
+            
+        }
+        return ans;
+    }
+    
+    public int solution(int[] info, int[][] edges) {
+        int graphLength = info.length;
+        
+        for(int i=0;i<graphLength;i++){
             graph.add(new ArrayList<>());
         }
-        for (int i = 0; i < edges.length; i++) {
+        
+        int edgeLength = edges.length;
+        for(int i=0;i<edgeLength;i++){
             graph.get(edges[i][0]).add(edges[i][1]);
         }
-
+        
         List<Integer> st = new ArrayList<>();
         for (int i : graph.get(0)) {
             st.add(i);
         }
+        
         return dfs(0, 0, 0, st, info);
     }
 }
