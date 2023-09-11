894d9136a6ab1d5d7750d478337f40898d660373
Han Ji Chan <gkswlcjs2@naver.com>
2023-09-12 07:22:55 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 5.07 ms, Memory: 96.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    int [][] graph;
+    Map<Integer, Integer> hashMap = new HashMap<>();
+    public void dfs(int y, int x, int lastY, int lastX, int size){
+        boolean checkFlag = true;
+        int tmpNumber = graph[y][x];
+        /* Check Quad (Total)*/
+        for(int i=y;i<lastY;i++){
+            for(int j=x;j<lastX;j++){
+                if(tmpNumber != graph[i][j]) {
+                    checkFlag = false;
+                    break;
+                }
+            }
+            if(!checkFlag) break;
+        }
+        if(checkFlag) {
+            hashMap.put(tmpNumber,hashMap.get(tmpNumber)+1);
+            return;
+        }
+        
+        if(size==2){
+            for(int i=y;i<lastY;i++){
+                for(int j=x;j<lastX;j++){
+                    hashMap.put(graph[i][j],hashMap.get(graph[i][j])+1);
+                }
+            }
+            return;
+        }
+
+
+        dfs(y,x,(y+lastY)/2,(x+lastX)/2, size/2);
+        dfs((y+lastY)/2,x,lastY,(lastX+x)/2, size/2);
+        dfs(y,(x+lastX)/2,(y+lastY)/2,lastX, size/2);
+        dfs((y+lastY)/2,(x+lastX)/2, lastY, lastX, size/2);
+
+    }
+
+    public int[] solution(int[][] arr) {
+        int[] answer = new int [2];
+        hashMap.put(0,0);
+        hashMap.put(1,0);
+        graph = arr;
+        dfs(0,0,arr[0].length,arr[0].length, arr[0].length);
+        answer[0] = hashMap.get(0);
+        answer[1] = hashMap.get(1);
+        return answer;
+    }
+}
