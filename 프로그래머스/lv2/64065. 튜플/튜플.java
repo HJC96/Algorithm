78e692146a74939814218e7b0810a99dcb67bc8e
Han Ji Chan <gkswlcjs2@naver.com>
2023-09-13 15:57:06 +0900

    [level 2] Title: 튜플, Time: 0.80 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -1,48 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-
 class Solution {
     public int[] solution(String s) {
-        
-        List<List<Integer>> parsedData = parseData(s);
-        
-        Collections.sort(parsedData, (a,b)->{
-            return Integer.compare(a.size(), b.size());
-        });
-        // Set<Integer> se = new HashSet<>();
-        List<Integer> ans = new ArrayList<>();
-        for(List<Integer> lis:parsedData){
-            for(int i:lis){
-                if(ans.contains(i) == false)
-                    ans.add(i);
-            }
-        }
-        int idx=0;
-        int[] answer = new int[ans.size()];
-        for(int i:ans) answer[idx++] = i;
+        int[] answer = {};
+        int[][] parsedData = parseData(s);
+
         return answer;
     }
-    public static List<List<Integer>> parseData(String data) {
+    
+    public static int[][] parseData(String data) {
         // 바깥쪽 중괄호 제거
         data = data.substring(1, data.length() - 1);
         
         // 각 그룹을 분리
         String[] groups = data.split("\\},\\{");
         
-        List<List<Integer>> resultList = new ArrayList<>();
+        ArrayList<int[]> resultList = new ArrayList<>();
         for (String group : groups) {
             group = group.replace("{", "").replace("}", "");
             String[] numbers = group.split(",");
-            
-            List<Integer> intList = new ArrayList<>();
-            for (String number : numbers) {
-                intList.add(Integer.parseInt(number));
+            int[] intArray = new int[numbers.length];
+            for (int i = 0; i < numbers.length; i++) {
+                intArray[i] = Integer.parseInt(numbers[i]);
             }
-            
-            resultList.add(intList);
+            resultList.add(intArray);
         }
 
-        return resultList;
+        return resultList.toArray(new int[0][]);
     }
 }
