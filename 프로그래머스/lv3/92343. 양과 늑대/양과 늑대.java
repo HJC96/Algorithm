94ae9de6ecae28b5ff60f331da7958eb53cdf52a
Han Ji Chan <gkswlcjs2@naver.com>
2023-09-28 17:38:04 +0900

    [level 3] Title: 양과 늑대, Time: 1.90 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -2,13 +2,14 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    List<List<Integer>> graph = new ArrayList<>();
     int answer = 0;
-    Map<Integer, List<Integer>> hashmap = new HashMap<>(); 
-    void dfs(int wolf, int sheep, int curNode, List<Integer> nextNodes, int[] info){
+    void choose(int wolf, int sheep, int curNode, List<Integer> nextNodes, int[] info){
         if(info[curNode] == 0) sheep++;
         else wolf++;
-        if(wolf>=sheep){
-            answer = Math.max(answer, sheep);
+        answer = Math.max(answer, sheep);
+        if(wolf >=sheep) {
+            // answer = Math.max(answer, sheep);
             return;
         }
         
@@ -16,33 +17,23 @@ class Solution {
             int nextNode = nextNodes.get(i);
             List<Integer> stackNodes = new ArrayList<>(nextNodes);
             stackNodes.remove((Integer)nextNode);
-            for(int num:hashmap.get(nextNode))
-                stackNodes.add(num);   
-            dfs(wolf,sheep,nextNode,stackNodes,info);
+            stackNodes.addAll(graph.get(nextNode));
+            choose(wolf, sheep, nextNode, stackNodes, info);
         }
-        answer = Math.max(answer, sheep);
+        
     }
-    
     public int solution(int[] info, int[][] edges) {
         
-        /* Make Graph */
-        int len = info.length;
-        for(int i=0;i<len;i++)
-            hashmap.put(i, new ArrayList<>());
+        for(int i=0;i<info.length;i++){
+            graph.add(new ArrayList<>());
+        }
+            
         for(int[] e:edges){
-            if(hashmap.get(e[0]) == null){
-                List<Integer> lis = new ArrayList<>();
-                lis.add(e[1]);
-                hashmap.put(e[0],lis);
-            }else{
-                hashmap.get(e[0]).add(e[1]);
-            }
+            graph.get(e[0]).add(e[1]);
         }
         List<Integer> nextNodes = new ArrayList<>();
-        nextNodes.addAll(hashmap.get(0));
-        /* Logic */
-        dfs(0,0,0,nextNodes,info);
-        
+        nextNodes.addAll(graph.get(0));
+        choose(0, 0, 0, nextNodes, info);
         
         return answer;
     }
