b3d8bf8dd4f9b4c069e936b5f6db50b481f3d9de
Han Ji Chan <gkswlcjs2@naver.com>
2023-10-07 09:15:47 +0900

    [level 3] Title: 입국심사, Time: 0.36 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43238. 입국심사/README.md
+++ 프로그래머스/lv3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 71.7 MB, 시간: 0.39 ms
+메모리: 71.6 MB, 시간: 0.36 ms
 
 ### 구분
 
--- 프로그래머스/lv3/43238. 입국심사/입국심사.java
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -3,30 +3,31 @@ import java.util.*;
 
 public class Solution {
     public long solution(int n, int[] times) {
+        long answer = 1;
         int length = times.length;
         Arrays.sort(times);
 
-        long maxTime = (long) times[length - 1] * n; // 가장 느린 사람에게 모든 일을 주는 경우
-        long minTime = 1; // 최소 시간은 1
-        long answer = maxTime; // 초기화
+        long maxTime = (long)times[0]*n;
+        long minTime = 1;
 
-        while (minTime <= maxTime) {
-            long averageTime = (minTime + maxTime) / 2;
+        while(minTime<=maxTime){
+            long averageTime = (minTime + maxTime)/2;
             long tmp_n = 0;
 
-            for (int i = 0; i < length; i++) {
+            for(int i=0;i<length;i++){
                 tmp_n += averageTime / times[i];
-                if (tmp_n >= n) break; // n을 초과하면 더 이상 더할 필요 없음
+                if(tmp_n > n) break;
             }
 
-            if (tmp_n < n) {
+            if(n > tmp_n){
                 minTime = averageTime + 1;
-            } else {
-                answer = Math.min(answer, averageTime); // 가능한 답 중 최소값을 저장
+            }
+            else{
                 maxTime = averageTime - 1;
             }
         }
 
-        return answer;
+
+        return minTime;
     }
-}
+} 
