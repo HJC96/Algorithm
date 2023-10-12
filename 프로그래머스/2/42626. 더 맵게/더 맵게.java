2758582d1f779b3e29524612e918a8dc0f4e958b
Han Ji Chan <gkswlcjs2@naver.com>
2023-10-13 07:06:47 +0900

    [level 2] Title: 더 맵게, Time: 1393.60 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for(int i:scoville) pq.add(i);
+        while(pq.peek() < K){
+            int num1 = pq.poll();
+            int num2 = pq.poll();
+            int num = num1 + num2*2;
+            answer++;
+            if(pq.isEmpty()) { if(num<K) answer=-1;break;}
+            pq.add(num);
+        }
+        return answer;
+    }
+}
