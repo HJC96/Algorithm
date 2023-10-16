39dc2a5a95723a8d0995203bf1af017465dbd592
Han Ji Chan <gkswlcjs2@naver.com>
2023-10-17 05:06:31 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.util.*;
+
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[]{0,0};
+        int sum = brown+yellow;
+        Map<Integer, Integer> m = new HashMap<>();
+            for(int i=3;i<=Math.sqrt(sum);i++){
+              if(sum % i==0){
+                  m.put(sum/i,i);
+                 
+              }
+            }
+        for(Map.Entry<Integer, Integer> h:m.entrySet()){
+            int a = h.getKey();
+            int b = h.getValue();
+            if((a-2) * (b-2) == yellow && (a+b-2)*2 ==brown) {answer[0] =a; answer[1] =b; }
+        }
+        return answer;
+    }
+}
