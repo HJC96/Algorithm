70cbb48c5c2d0c3acdfa35921848e8a434676a7e
Han Ji Chan <gkswlcjs2@naver.com>
2024-08-20 06:04:33 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 136 ms, Memory: 16148 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        int c = Integer.parseInt(arr[2]);
+        int d = Integer.parseInt(arr[3]);
+        int e = Integer.parseInt(arr[4]);
+        int f = Integer.parseInt(arr[5]);
+        for(int i=-999;i<=999;i++){
+            for(int j=-999;j<=999;j++){
+                if((i*a + j*b == c) && (i*d + j*e == f)){
+                    System.out.println(i+" "+j);
+                    return ;
+                }
+            }
+        }
+    }
+}
+
