7205db48be85d315cda62c71bd0a6f926c940fc5
Han Ji Chan <gkswlcjs2@naver.com>
2024-08-23 06:15:50 +0900

    [Silver II] Title: 연속합, Time: 236 ms, Memory: 25284 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,29 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int n = Integer.parseInt(str);
-        String[] array = br.readLine().split(" ");
-        if(n == 1) {System.out.println(array[0]); return;}
-        int length = n;
-        int[] arr = new int[length];
-        long[] dp = new long[length];
-        for (int i = 0; i < length; i++) arr[i] = Integer.parseInt(array[i]);
-//        if(arr[0] > 0)
-            dp[0] = arr[0];
-//        else dp[0] = 0;
-
-        long answer = arr[0];
-        for(int i=1;i<n;i++) {
-            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
-            answer = Math.max(answer, dp[i]);
+        String input = br.readLine();
+        int N = Integer.parseInt(input);
+        String[] numbers = br.readLine().split(" ");
+        int [] sum = new int[N+1]; // 앞 한개 버퍼
+        int answer = Integer.MIN_VALUE;
+        for(int i=1;i<N+1;i++){
+            int num = sum[i-1] + Integer.parseInt(numbers[i-1]);
+            if(num<=0){
+                sum[i] = 0;
+            }else{
+                sum[i] = sum[i-1] + Integer.parseInt(numbers[i-1]);
+            }
+            answer = Math.max(answer, num);
         }
-
-
         System.out.println(answer);
     }
 }
