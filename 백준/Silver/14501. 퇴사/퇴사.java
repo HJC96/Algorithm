68e28922890255e88fc18768cf7658730ffcf415
Han Ji Chan <gkswlcjs2@naver.com>
2024-08-30 06:07:32 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -13,28 +13,29 @@ public class Main {
     private static int N;
     public static void main(String[] args) throws IOException
     {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            N = Integer.parseInt(br.readLine());
-            numbers = new int[N][2];
-            for(int i=0;i<N;i++){
-                String[] input = br.readLine().split(" ");
-                numbers[i][0] = Integer.parseInt(input[0]);
-                numbers[i][1] = Integer.parseInt(input[1]);
-            }
-            curr(0, 0);
-
-            System.out.println(answer);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        numbers = new int[N][2];
+        for(int i=0;i<N;i++){
+            String[] input = br.readLine().split(" ");
+            numbers[i][0] = Integer.parseInt(input[0]);
+            numbers[i][1] = Integer.parseInt(input[1]);
         }
+        curr(0, 0);
 
-        private static void curr(int day, int money){
-            if(day >= N){
-                answer = Math.max(answer, money);
-                return;
-            }
-            if(day+numbers[day][0] <= N) {
-                curr(day + numbers[day][0], money + numbers[day][1]); // 해당 날짜 선택시
-            }
-            curr(day+1, money);
-        }
+        System.out.println(answer);
     }
+
+    private static void curr(int day, int money){
+        if(day == N){
+            answer = Math.max(answer, money);
+            return;
+        }
+        if(day > N){
+            return;
+        }
+        curr(day + numbers[day][0], money + numbers[day][1]); // 해당 날짜 선택시
+        curr(day+1, money);
+    }
+}
 
