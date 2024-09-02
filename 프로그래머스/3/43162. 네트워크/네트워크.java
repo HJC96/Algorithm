bf1ca3bafed46304c56f73d3e15852d488fe0f79
Han Ji Chan <gkswlcjs2@naver.com>
2024-09-03 06:00:29 +0900

    [level 3] Title: 네트워크, Time: 0.97 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,57 +2,44 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    boolean [] visited;
-    List<List<Integer>> computer;
-    int answer = 1;
-    boolean connectedCheck = true;
+    ArrayList<Integer> []graph;
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
-
-        visited = new boolean[computers[0].length];
-
-        computer = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            computer.add(new ArrayList<>());
+        int answer = 0;
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        for(int i=0;i<=n;i++){
+            graph[i] = new ArrayList<Integer>();
         }
-
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
+                if(i==j) continue;
                 if(computers[i][j] == 1){
-                    computer.get(i).add(j);
+                    graph[i+1].add(j+1);
+                    graph[j+1].add(i+1);
                 }
             }
         }
         
-        for(int i=0;i<n;i++){
-            dfs(i);
-            for(int j=0;j<n;j++){
-                if(visited[j] == false && connectedCheck == true) {
-                    answer++;
-                    break;
-                }
+        
+        for(int i=1;i<=n;i++){
+            if(!visited[i]){
+                dfs(i);
+                answer++;
             }
-            connectedCheck = false;
         }
+        
         return answer;
     }
-    public void dfs(int num){
-        if(visited[num]) return;
-        visited[num] = true;
-        for(int i:computer.get(num)){
-            dfs(i);
+    
+    void dfs(int node){
+        visited[node] = true;
+        for(int nxt:graph[node]){
+            if(visited[nxt]){
+                continue;
+            }
+            dfs(nxt);
         }
-        connectedCheck = true;
     }
 }
-
-/*
-    개념: 타고타고 들어가서 카운트 하는거
-    for 문으로 계속 돌리고, 방문 처리해서 방문 했으면 방문 안하고 방문 안했을면 방문 하면 됨.
-    1. 각 노드에서 연결 관계를 표시해야 한다.
-    2. 그 연결관계를 이용해서, 방문한다
-    3. 방문했다면, 체크하고
-        1.
-        2.
-        3.
-
-*/
