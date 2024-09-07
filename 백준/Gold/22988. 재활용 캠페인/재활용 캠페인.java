967540949968d2490d6003c69ee7afdd4f8ab54a
Han Ji Chan <gkswlcjs2@naver.com>
2024-09-07 12:34:20 +0900

    [Gold II] Title: 재활용 캠페인, Time: 416 ms, Memory: 35608 KB -BaekjoonHub

--- 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.java
+++ 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.java
@@ -16,41 +16,36 @@ public class Main {
             arr[i] = Long.parseLong(line[i]);
         }
 
-        // 배열을 오름차순으로 정렬
         Arrays.sort(arr);
 
         int s = 0;
         int e = N - 1;
-        int remain = 0;  // 남은 용기 수
-        int cnt = 0;     // 꽉 찬 용기의 수
+        int remain = 0;  
+        int cnt = 0; 
 
         while (s <= e) {
-            // 큰 용기가 Xml일 경우 바로 처리
             if (arr[e] == X) {
                 cnt++;
                 e--;
                 continue;
             }
 
-            // 남은 용기가 하나일 경우
             if (s == e) {
                 remain++;
                 break;
             }
 
-            // 두 용기를 합쳐서 조건 만족 여부 확인 (double로 처리)
+            // 두 용기를 합쳐서 조건 만족 여부 확인 (double로 처리, 정수처리되면 제대로 계산 안됨.)
             if (arr[e] + arr[s] >= (double) X / 2) {
                 cnt++;
                 s++;
                 e--;
             } else {
-                // 용량이 모자라면 작은 용기만 남김
                 s++;
                 remain++;
             }
         }
 
-        // 남은 용기들을 3개씩 묶어 추가로 꽉 찬 용기를 만듦
         cnt += remain / 3;
         System.out.println(cnt);
     }
