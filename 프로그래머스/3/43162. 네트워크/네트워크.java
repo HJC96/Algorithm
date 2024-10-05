db542d381075cfb7a3429d5582be2a576931e685
Han Ji Chan <gkswlcjs2@naver.com>
2024-10-05 15:47:52 +0900

    [level 3] Title: 네트워크, Time: 0.74 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,44 +2,36 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    ArrayList<Integer> []graph;
-    boolean[] visited;
+    private List<List<Integer>> g = new ArrayList<>();
+    private static boolean[] visited = new boolean[201];
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        graph = new ArrayList[n+1];
-        visited = new boolean[n+1];
-        for(int i=0;i<=n;i++){
-            graph[i] = new ArrayList<Integer>();
+        for(int i=0;i<201;i++){
+            g.add(new ArrayList<>());
         }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<computers[0].length;i++){
+            for(int j=0;j<computers[0].length;j++){
                 if(i==j) continue;
                 if(computers[i][j] == 1){
-                    graph[i+1].add(j+1);
-                    graph[j+1].add(i+1);
+                    g.get(i).add(j);
                 }
             }
         }
-        
-        
-        for(int i=1;i<=n;i++){
-            if(!visited[i]){
-                dfs(i);
-                answer++;
-            }
+        for(int i=0;i<computers[0].length;i++){
+            if(visited[i]) continue;
+            dfs(i);
+            answer++;
         }
-        
         return answer;
     }
     
-    void dfs(int node){
-        visited[node] = true;
-        for(int nxt:graph[node]){
-            if(visited[nxt]){
-                continue;
+    private void dfs(int p){
+        if(!visited[p]){
+            visited[p] = true;
+            for(int i:g.get(p)){
+                dfs(i);
             }
-            dfs(nxt);
         }
+        return ;
     }
 }
