42d9fc4c90e686bf316debc6bfa5e5e46f83f567
Han Ji Chan <gkswlcjs2@naver.com>
2024-10-05 15:47:44 +0900

    [level 2] Title: 타겟 넘버, Time: 6.69 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 5.93 ms
+메모리: 83.4 MB, 시간: 6.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 02일 06:57:50
+2024년 10월 05일 15:47:41
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,23 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
-    int[] nums;
-    int T;
-    int ans=0;
+    private static int answer = 0;
+    private static int t = 0;
     public int solution(int[] numbers, int target) {
-        nums = numbers;
-        T = target;
-        dfs(0, 0);
-        return ans;
+        t = target;
+        curr(numbers, 0, 0);
+        return answer;
     }
     
-    void dfs(int idx, int sum){
-        if(sum == T && nums.length == idx){
-            ans++;
-            return;
+    private void curr(int[] numbers, int current, int sum){
+        if(current == numbers.length){
+            if(sum == t){
+                answer++;
+            }
+            return ;
         }
-        
-        if(idx == nums.length){
-            return;
-        }
-        
-        dfs(idx+1, sum+nums[idx]);
-        dfs(idx+1, sum-nums[idx]);
+        curr(numbers, current+1, sum + numbers[current]);
+        curr(numbers, current+1, sum - numbers[current]);
     }
-    
 }
