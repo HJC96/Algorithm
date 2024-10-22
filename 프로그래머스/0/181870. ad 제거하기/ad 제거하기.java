b9be6a6d635013eb410cb56fbd28a54d85df25d9
Han Ji Chan <gkswlcjs2@naver.com>
2024-10-22 21:15:46 +0900

    [level 0] Title: ad 제거하기, Time: 4.22 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+
+class Solution {
+    public String[] solution(String[] strArr) {
+    
+        return Arrays.stream(strArr)
+            .filter(s->!s.contains("ad"))
+            .toArray(String[]::new);
+    }
+}
