82bda10dc492e44d7a5f652ff2bc8936a18f2c3f
Han Ji Chan <gkswlcjs2@naver.com>
2024-10-31 06:48:59 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.04 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, String alp) {
+        return my_string.replace(alp, alp.toUpperCase());
+    }
+}
